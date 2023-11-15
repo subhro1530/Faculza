18b305206e54ec5760b70d98fe44fdb7e226d77b
subhro1530 <shaswata.ssaha@gmail.com>
2023-11-15 23:30:03 +0530

    ready for deployment, changes 1

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerIcon = document.querySelector(".hamburger-icon");
+  const sidebar = document.querySelector(".sidebar");
+
+  if (hamburgerIcon) {
+    hamburgerIcon.addEventListener("click", function () {
+      sidebar.classList.toggle("show");
+    });
+  }
+
+  // JavaScript for animating text in the hero section
+  const heroContent = document.querySelector(".hero-content h1");
+
+  function animateText() {
+    const text = "Faculza: Simplify SaaS project success";
+    let index = 0;
+
+    function displayText() {
+      heroContent.textContent = text.slice(0, index);
+      index++;
+
+      if (index > text.length) {
+        clearInterval(animationInterval);
+      }
+    }
+
+    const animationInterval = setInterval(displayText, 150);
+  }
+
+  if (heroContent) {
+    animateText();
+  }
+});
